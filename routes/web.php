64e8e3af2cb2d2a4f3b403297217a8d524fc6892
DokuDokuringo <153196181+Ringo0803Ringo@users.noreply.github.com>
2024-02-11 23:14:16 +0900

    topヘージ基礎作成

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TopController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TopController::class, 'get_top'])->name('top');
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::middleware(['auth', 'verified'])->group(function () {
+
+
+
+});

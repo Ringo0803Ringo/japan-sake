15dcee107151fde4dbbad25db37c511307317c52
DokuDokuringo <153196181+Ringo0803Ringo@users.noreply.github.com>
2024-03-16 20:57:21 +0900

    銘柄データAPI挿入

---
+++ app/Console/Commands/FetchBrands.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use GuzzleHttp\Client;
+use App\Models\Brand;
+
+class FetchBrands extends Command
+{
+    protected $signature = 'fetch:brands';
+    protected $description = 'Fetch brands from external API and store them in the database';
+
+    public function handle()
+    {
+        $client = new Client();
+        $response = $client->request('GET', 'https://muro.sakenowa.com/sakenowa-data/api/brands');
+        $brands = json_decode($response->getBody()->getContents(), true);
+
+        foreach ($brands['brands'] as $brand) {
+            Brand::updateOrCreate(['id' => $brand['id']], ['name' => $brand['name'], 'breweryId' => $brand['breweryId']]);
+        }
+
+        $this->info('Brands have been fetched and stored successfully.');
+    }
+}

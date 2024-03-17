b50c30f1784d6b9d0b37dc8a672e057867130fa3
DokuDokuringo <153196181+Ringo0803Ringo@users.noreply.github.com>
2024-03-17 15:24:25 +0900

    蔵元のAPIデータを保存

---
+++ app/Console/Commands/FetchBreweries.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use GuzzleHttp\Client;
+use App\Models\Brewery;
+
+class FetchBreweries extends Command
+{
+    protected $signature = 'fetch:breweries';
+    protected $description = 'Fetch breweries from external API and store them in the database';
+
+    public function handle()
+    {
+        $client = new Client();
+        $response = $client->request('GET', 'https://muro.sakenowa.com/sakenowa-data/api/breweries');
+        $breweries = json_decode($response->getBody()->getContents(), true);
+
+        foreach ($breweries['breweries'] as $brewery) {
+            Brewery::updateOrCreate(['id' => $brewery['id']], ['name' => $brewery['name'], 'areaId' => $brewery['areaId']]);
+        }
+
+        $this->info('Breweries have been fetched and stored successfully.');
+    }
+}

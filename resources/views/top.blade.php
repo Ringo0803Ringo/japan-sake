eab5c59c8abe2e7f013f1b9d7a3d6dfb49cefe94
DokuDokuringo <153196181+Ringo0803Ringo@users.noreply.github.com>
2024-05-29 16:13:10 +0900

    オススメ銘柄の表示

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -9,38 +9,45 @@
 <div class="container">
     <div class="row">
         <h2 class="text-center font-color"><span class=" highlight">日々の晩酌に彩りを与えよう！</span></h2>
-        <div class="swiper-container">
-            <div class="swiper-wrapper">
-                @foreach ($photos as $photo)
-                <div class="swiper-slide">
-                    <img src="data:image/jpeg;base64,{{ $photo->filename }}" alt="Uploaded Image" class="responsive">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header h4">⭐️オススメ銘柄</div>
+                <div class="card-body">
+                    <div class="swiper-container">
+                        <div class="swiper-wrapper">
+                            @foreach ($photos as $photo)
+                            <div class="swiper-slide">
+                                <img src="data:image/jpeg;base64,{{ $photo->filename }}" alt="Uploaded Image" class="responsive mb-3">
+                            </div>
+                            @endforeach
+                        </div>
+                        <!-- Add Pagination -->
+                        <div class="swiper-pagination"></div>
+                        <!-- Add Navigation -->
+                        <div class="swiper-button-prev"></div>
+                        <div class="swiper-button-next"></div>
+                    </div>
+                    <script>
+                        document.addEventListener('DOMContentLoaded', function () {
+                            var mySwiper = new Swiper('.swiper-container', {
+                                loop: true,
+                                slidesPerView: 1,
+                                spaceBetween: 500,
+                                pagination: {
+                                    el: '.swiper-pagination',
+                                    clickable: true,
+                                },
+                                navigation: {
+                                    nextEl: '.swiper-button-next',
+                                    prevEl: '.swiper-button-prev',
+                                },
+                            });
+                        });
+                    </script>
                 </div>
-                @endforeach
             </div>
-            <!-- Add Pagination -->
-            <div class="swiper-pagination"></div>
-            <!-- Add Navigation -->
-            <div class="swiper-button-prev"></div>
-            <div class="swiper-button-next"></div>
         </div>
-        <script>
-            document.addEventListener('DOMContentLoaded', function () {
-                var mySwiper = new Swiper('.swiper-container', {
-                    loop: true,
-                    slidesPerView: 1,
-                    spaceBetween: 500,
-                    pagination: {
-                        el: '.swiper-pagination',
-                        clickable: true,
-                    },
-                    navigation: {
-                        nextEl: '.swiper-button-next',
-                        prevEl: '.swiper-button-prev',
-                    },
-                });
-            });
-        </script>
-        <div class="col-md-8">
+        <div class="col-md-8 mt-5">
             <div class="card mt-3">
                 <div class="card-header h4">銘柄検索</div>
                 <div class="card-body">
@@ -52,43 +59,28 @@
             </div>
 
             <div class="card mt-3 bottom-space">
-                {{-- <button class="btn btn-brand" id="toggleButton">銘柄一覧を表示</button>
-                <div id="collapsibleContent" style="display: none;"> --}}
-                    <div class="card-header h4 d-flex align-items-center">
-                        <span>銘柄一覧</span>
-                        <form action="{{ route('order') }}" method="GET" class="d-flex align-items-center ms-4">
-                            <select class="form-control-sm mt-1" name="sort" onchange="this.form.submit()">
-                                <option value="">銘柄並べ替え</option>
-                                <option value="name_asc">名前順（昇順）</option>
-                                <option value="name_desc">名前順（降順）</option>
-                                <!-- 他の並べ替え基準を追加 -->
-                            </select>
+                <div class="card-header h4 d-flex align-items-center">
+                    <span>銘柄一覧</span>
+                    <form action="{{ route('order') }}" method="GET" class="d-flex align-items-center ms-4">
+                        <select class="form-control-sm mt-1" name="sort" onchange="this.form.submit()">
+                            <option value="">銘柄並べ替え</option>
+                            <option value="name_asc">名前順（昇順）</option>
+                            <option value="name_desc">名前順（降順）</option>
+                            <!-- 他の並べ替え基準を追加 -->
+                        </select>
 
-                        </form>
-                    </div>
-                    <div class="card-body">
-                        @foreach ($brands as $brand)
-                        <li><a href="{{ route('brand.show', $brand->id) }}">{{ $brand->name }}</a></li>
-                        @endforeach
-                        <ul class="pagination pagination-sm mt-3">{{ $brands->appends(request()->query())->links() }}</ul>
-                    </div>
-                {{-- </div>
-                <script>
-                    document.getElementById('toggleButton').addEventListener('click', function() {
-                        var content = document.getElementById('collapsibleContent');
-                        if (content.style.display === 'none') {
-                            content.style.display = 'block';
-                            this.textContent = '銘柄一覧を隠す';
-                        } else {
-                            content.style.display = 'none';
-                            this.textContent = '銘柄一覧を表示';
-                        }
-                    });
-                </script> --}}
+                    </form>
+                </div>
+                <div class="card-body">
+                    @foreach ($brands as $brand)
+                    <li><a href="{{ route('brand.show', $brand->id) }}">{{ $brand->name }}</a></li>
+                    @endforeach
+                    <ul class="pagination pagination-sm mt-3">{{ $brands->appends(request()->query())->links() }}</ul>
+                </div>
             </div>
         </div>
 
-        <div class="col-md-4 sidebar">
+        <div class="col-md-4 mt-5 sidebar">
             <div class="card mt-3">
                 <div class="card-header h5">銘柄ランキング</div>
                 <div class="card-body">

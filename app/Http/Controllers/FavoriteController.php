ee8277e6291b7bfce1c37c293763725977e22a78
DokuDokuringo <153196181+Ringo0803Ringo@users.noreply.github.com>
2024-08-06 23:24:04 +0900

    セッションフラッシュメッセージの文言の変更

--- app/Http/Controllers/FavoriteController.php
+++ app/Http/Controllers/FavoriteController.php
@@ -23,6 +23,6 @@ class FavoriteController extends Controller
         $brand = Brand::find($request->brand_id);
         $brand->favorites()->where('user_id', Auth::user()->id)->delete();
 
-        return back()->with('success', 'お気に入り解除しました');
+        return back()->with('success', 'お気に入り解除しました。');
     }
 }
